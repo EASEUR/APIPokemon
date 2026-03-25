fe43ef4d1818aa72ab82005d9887533c3622871e
voscar <oscar.vivien@ynov.com>
2026-03-25 16:22:11 +0100

    jsp

--- api/index.js
+++ api/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const app = express();
-
-// IMPORTANT : exporter un handler compatible Vercel
 const serverless = require('serverless-http');
 
-const pokemons = require('../db-pokemons');
-const { /* tes helpers */ } = require('../helper');
+const app = express();
 
-// ici tu remets ton code Express actuel (routes, middlewares, etc.)
-// exemple :
+// TODO: remettre tes vraies données/routes plus tard
+// const pokemons = require('../db-pokemons');
+// const helpers = require('../helper');
+
 app.get('/', (req, res) => {
   res.send('API Pokemon OK');
 });
 
-// routes existantes, par ex. /api/pokemons, etc.
-
-// Vercel attend une fonction, pas app.listen
-module.exports = app;        // si tu veux adapter différemment
-module.exports.handler = serverless(app);
+// IMPORTANT : exporter un handler compatible Vercel
+module.exports = serverless(app);
