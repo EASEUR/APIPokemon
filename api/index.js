9e97ec363c83f48a3902d60db3bc5055d3e5c418
voscar <oscar.vivien@ynov.com>
2026-03-25 16:24:45 +0100

    jsp

--- api/index.js
+++ api/index.js
@@ -1,15 +1,5 @@
-const express = require('express');
-const serverless = require('serverless-http');
+// api/index.js
 
-const app = express();
-
-// TODO: remettre tes vraies données/routes plus tard
-// const pokemons = require('../db-pokemons');
-// const helpers = require('../helper');
-
-app.get('/', (req, res) => {
-  res.send('API Pokemon OK');
-});
-
-// IMPORTANT : exporter un handler compatible Vercel
-module.exports = serverless(app);
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'API Pokemon OK minimal' });
+};
